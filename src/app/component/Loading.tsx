4ffdd4f1687197c424babcb19c520084a6130776
panntod <pandhu.munjalindra@gmail.com>
2024-03-23 08:29:33 +0700

    feat: added component loading

---
+++ src/app/component/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="flex justify-center items-center h-screen w-screen">
+            <span className="loading loading-ring loading-lg text-primary"></span>
+        </div>
+    )
+}
