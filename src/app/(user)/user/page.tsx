3ce89363a745bed84b16c3cc990140b21b8c6763
panntod <pandhu.munjalindra@gmail.com>
2024-04-23 06:07:49 +0700

    refactor: Simply code

--- src/app/(user)/user/page.tsx
+++ src/app/(user)/user/page.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
-import { deleteUser } from "@/actions/user.actions";
 import { Card } from "@/app/component/Card";
-import { toast } from "sonner";
 import { PrismaClient } from "@prisma/client";
 
 
@@ -9,21 +7,12 @@ export default async function Home() {
   const prisma = new PrismaClient()
   const data = await prisma.user.findMany({
     select: {
-        id: true,
-        name: true,
-        email: true,
-        kelas: true,
+      id: true,
+      name: true,
+      email: true,
+      kelas: true,
     },
-});
-
-  const handleDelete = async (id: string) => {
-    const response = await deleteUser(id);
-    if (response.success == true) {
-      toast.success(response.message);
-    } else if (response.success == false) {
-      toast.error(response.message);
-    }
-  }
+  });
 
   return (
     <>
@@ -33,7 +22,7 @@ export default async function Home() {
           <h2 className="text-4xl font-bold mb-4 text-center">User Details</h2>
           <Link className="btn mb-6 w-64 btn-primary text-white" href={"/tambah-user"}>Tambah User</Link>
           <div className="gap-6 mb-6 flex justify-center flex-wrap">
-            {data?.map((data) => <Card data={data} key={data.id} onDrop={() => { handleDelete(data.id) }} />)}
+            {data?.map((data) => <Card data={data} key={data.id} />)}
           </div>
         </div>
       </div>
