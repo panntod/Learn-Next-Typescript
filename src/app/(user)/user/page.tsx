da5650f013689996ffaa671fcb22a30c5ea78f50
panntod <pandhu.munjalindra@gmail.com>
2024-04-22 18:50:11 +0700

    refactor: Create this page server component

--- src/app/(user)/user/page.tsx
+++ src/app/(user)/user/page.tsx
@@ -1,27 +1,22 @@
-'use client'
-
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { deleteUser } from "@/actions/user.actions";
 import { UserInterface } from "@/types/User.type";
 import { Card } from "@/app/component/Card";
 import { toast } from "sonner";
-import Loading from "@/app/component/Loading";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
-  const [dataUser, setDataUser] = useState<UserInterface[]>()
-  const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    const result = await fetch("/user/api")
-    const data = await result.json()
-    setDataUser(data);
-    setLoading(false)
-  };
+export default async function Home() {
+  const prisma = new PrismaClient()
+  const data = await prisma.user.findMany({
+    select: {
+        id: true,
+        name: true,
+        email: true,
+        kelas: true,
+    },
+});
 
   const handleDelete = async (id: string) => {
     const response = await deleteUser(id);
@@ -30,25 +25,21 @@ export default function Home() {
     } else if (response.success == false) {
       toast.error(response.message);
     }
-
-    fetchData();
   }
 
   return (
     <>
-      {loading ? (
-        <Loading />
-      ) : (
-        <div className="w-full flex flex-col justify-center items-center pt-24 px-6">
-          <div className="w-[1280px]">
-            <h2 className="text-4xl font-bold mb-4 text-center">User Details</h2>
-            <Link className="btn mb-6 w-64 btn-primary text-white" href={"/tambah-user"}>Tambah User</Link>
-            <div className="gap-6 mb-6 flex justify-center flex-wrap">
-              {dataUser?.map((data) => <Card data={data} key={data.id} onDrop={() => { handleDelete(data.id) }} />)}
-            </div>
+
+      <div className="w-full flex flex-col justify-center items-center pt-24 px-6">
+        <div className="w-[1280px]">
+          <h2 className="text-4xl font-bold mb-4 text-center">User Details</h2>
+          <Link className="btn mb-6 w-64 btn-primary text-white" href={"/tambah-user"}>Tambah User</Link>
+          <div className="gap-6 mb-6 flex justify-center flex-wrap">
+            {data?.map((data) => <Card data={data} key={data.id} onDrop={() => { handleDelete(data.id) }} />)}
           </div>
         </div>
-      )}
+      </div>
+
     </>
   );
 }
