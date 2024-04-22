00ee5961c416d817b57b55e320a6bd2a1832ae20
panntod <pandhu.munjalindra@gmail.com>
2024-04-23 06:08:23 +0700

    feat: Create function handle delete and navigate edit

--- src/app/component/Card.tsx
+++ src/app/component/Card.tsx
@@ -3,9 +3,21 @@
 import Link from 'next/link';
 import { UserInterface } from '@/types/User.type'
 import { PrimaryButton, CloseButton } from './Button';
+import { deleteUser } from '@/actions/user.actions';
+import { toast } from 'sonner';
+import { navigateEdit } from '@/actions/redirect.actions';
 
-export const Card = ({ data, onDrop }: { data: UserInterface, onDrop: () => void }) => {
-    
+export const Card = ({ data }: { data: UserInterface }) => {
+
+    const handleDelete = async (id: string) => {
+        const response = await deleteUser(id);
+        if (response.success == true) {
+            toast.success(response.message);
+        } else if (response.success == false) {
+            toast.error(response.message);
+        }
+    }
+
     return (
         <>
             <div className='bg-slate-100 p-6 border rounded-2xl w-[24rem] h-[20rem] flex flex-col justify-between'>
@@ -15,9 +27,9 @@ export const Card = ({ data, onDrop }: { data: UserInterface, onDrop: () => void
                     <p className="mb-2 text-wrap">Email: {data.email}</p>
                     <p>Kelas: {data.kelas}</p>
                 </div>
-                <div>
-                    <Link className='btn mt-4 w-full btn-warning text-white' href={`/${data.id}/edit`}>Update</Link>
-                    <PrimaryButton onClick={()=>{const dialogElement = document.getElementById('modalDelete') as HTMLDialogElement; dialogElement.showModal()}}>Delete</PrimaryButton>
+                <div className='flex w-full gap-2 items-center'>
+                    <PrimaryButton className='btn-warning w-[48%]' onClick={() => navigateEdit(data.id)}>Update</PrimaryButton>
+                    <PrimaryButton className='btn-error w-[48%]' onClick={() => { const dialogElement = document.getElementById('modalDelete') as HTMLDialogElement; dialogElement.showModal() }}>Delete</PrimaryButton>
                 </div>
             </div>
             <dialog id="modalDelete" className="modal">
@@ -29,7 +41,7 @@ export const Card = ({ data, onDrop }: { data: UserInterface, onDrop: () => void
                     <p className="py-4">Apakah kamu yakin ingin menghapus data ini!</p>
                     <div className="modal-action">
                         <form method="dialog">
-                            <PrimaryButton  onClick={() => onDrop()}>Delete</PrimaryButton>
+                            <PrimaryButton className='btn-error' onClick={() => handleDelete(data.id)}>Delete</PrimaryButton>
                         </form>
                     </div>
                 </div>
