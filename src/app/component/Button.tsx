95edc69179aab088a9d18f5a0975eabeb5ab639b
panntod <pandhu.munjalindra@gmail.com>
2024-04-23 06:08:05 +0700

    feat: Added props class name

--- src/app/component/Button.tsx
+++ src/app/component/Button.tsx
@@ -3,48 +3,50 @@
 import { MouseEventHandler, ReactNode } from "react";
 
 interface ButtonProps {
-    children?: ReactNode;
-    type?: "button" | "reset" | "submit";
-    onClick?: MouseEventHandler<HTMLButtonElement>;
-    isDisabled?: boolean;
+  children?: ReactNode;
+  type?: "button" | "reset" | "submit";
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+  isDisabled?: boolean;
 }
 
 export function PrimaryButton({
-    children,
-    type,
-    onClick,
-  }: Readonly<ButtonProps>) {
-    return (
-      <button
-        type={type}
-        onClick={onClick}
-        className="btn btn-error"
+  children,
+  type,
+  onClick,
+  className
+}: Readonly<ButtonProps>) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`btn ${className}`}
+    >
+      <span
+        className={"text-base text-white"}
       >
-        <span
-          className={"text-base text-white"}
-        >
-          {children}
-        </span>
-      </button>
-    );
-  }
+        {children}
+      </span>
+    </button>
+  );
+}
 
-  export function CloseButton({
-    children,
-    type,
-    onClick,
-  }: Readonly<ButtonProps>) {
-    return (
-      <button
-        type={type}
-        onClick={onClick}
-        className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+export function CloseButton({
+  children,
+  type,
+  onClick,
+}: Readonly<ButtonProps>) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+    >
+      <span
+        className={"text-base text-white"}
       >
-        <span
-          className={"text-base text-white"}
-        >
-          {children}
-        </span>
-      </button>
-    );
-  }
+        {children}
+      </span>
+    </button>
+  );
+}
